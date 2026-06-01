8978b6206048c58503983f5059bae5f7f9d011da
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-01 16:49:53 +0400

    i finished problem2

--- Program.cs
+++ Program.cs
@@ -15,12 +15,67 @@
 
             Console.WriteLine("Total readings: " + temperatures.Length);
         }
-     static void Main(string[] args)
+        public static void score()
+        {
+            int[] scores = { 85, 92, 70, 60, 88, 75 };
+
+
+            Console.WriteLine("Original order: ");
+            foreach (int score in scores)
             {
-            temp1();
+                Console.WriteLine(score);
+            }
+            Array.Reverse(scores);
+            Console.WriteLine("Reversed order: ");
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
             }
         }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1.");
+            Console.WriteLine("2.");
+            Console.WriteLine("3.");
+            Console.WriteLine("4.");
+            Console.WriteLine("5.");
+            Console.WriteLine("6.");
+            Console.WriteLine("7.");
+            Console.WriteLine("8.");
+            Console.WriteLine("9.");
+            Console.WriteLine("10.");
+
+            Console.WriteLine("select option");
+            int option = int.Parse( Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    temp1();
+                    break;
+                case 2:
+                    score();
+                    break;
+                
+                    
+            }
+           
+
+            
+        }
+
+
+
+
+
+
+
+
     }
+}
+    
 
 
 
