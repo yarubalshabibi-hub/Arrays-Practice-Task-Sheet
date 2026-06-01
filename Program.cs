d67dab33f1fd5f1959924dfc3dec99c07596cb55
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-01 15:25:57 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arrays_Practice_Task_Sheet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
