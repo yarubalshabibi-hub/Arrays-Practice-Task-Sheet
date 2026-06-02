9e077ab4b9e9b9f1274d173e4f76744f7c2a8718
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-02 14:21:26 +0400

    I edite problem5

--- Program.cs
+++ Program.cs
@@ -37,30 +37,36 @@ namespace Arrays_Practice_Task_Sheet
 
 
 
-           public static void prices()
-        {
-            double[] prices = { 4.99, 12.50, 7.25, 19.99, 3.75 };
-
-
-            Console.WriteLine("Product prices:");
-            for (int i = 0; i < prices.Length; i++)
+        public static void prices()
             {
-                Console.WriteLine("Product " + (i + 1) + ": " + prices[i]);
+                double[] prices = { 4.99, 12.50, 7.25, 19.99, 3.75 };
+
+
+                Console.WriteLine("Product prices:");
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    Console.WriteLine("Product " + (i + 1) + ": " + prices[i]);
+                }
+
+
+                double target = 7.25;
+                int index = Array.IndexOf(prices, target);
+
+                if (index != -1)
+                {
+                    Console.WriteLine("Price " + target + " found at index " + index);
+                }
+                  
+                else
+                {
+                    Console.WriteLine("Price " + target + " not found");
+                }
+                    
             }
 
 
-            double target = 7.25;
-            int index = Array.IndexOf(prices, target);
 
-            if (index != -1)
-                Console.WriteLine("Price " + target + " found at index " + index);
-            else
-                Console.WriteLine("Price " + target + " not found");
-        }
-
-
-
-            public static void FinishTimes()
+        public static void FinishTimes()
         {
             int[] finishTimes = { 312, 285, 340, 298, 265, 320, 275, 355 };
 
@@ -97,80 +103,67 @@ namespace Arrays_Practice_Task_Sheet
                 Console.WriteLine("Rank " + (i + 1) + ": " + grades[i]);
 
             }
-
-
-
-            static void Main(string[] args)
-            {
-
-
-                Console.WriteLine("select one option please: ");
-                Console.WriteLine("1.");
-                Console.WriteLine("2.");
-                Console.WriteLine("3.");
-                Console.WriteLine("4.");
-                Console.WriteLine("5.");
-                Console.WriteLine("6.");
-                Console.WriteLine("7.");
-                Console.WriteLine("8.");
-                Console.WriteLine("9.");
-                Console.WriteLine("10.");
-            }
-
-            { 
-                int option = int.Parse(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        temp1();
-                        break;
-
-                    case 2:
-                        score();
-                        break;
-
-                    case 3:
-                        prices();
-                        break;
-
-                    case 4:
-                        FinishTimes();
-                        break;
-
-                    case 5:
-                        grade();
-                        break;
-
-                    case 6:
-                        break;
-
-                    case 7:
-                        break;
-
-                    case 8:
-                        break;
-
-                    case 9:
-                        break;
-
-                    case 10:
-                        break;
-                   
-
-                }
-
-
-
-            }
-
-
-
-
-
-
         }
 
+        static void Main(string[] args)
+        {
 
+
+            Console.WriteLine("select one option please: ");
+            Console.WriteLine("1.");
+            Console.WriteLine("2.");
+            Console.WriteLine("3.");
+            Console.WriteLine("4.");
+            Console.WriteLine("5.");
+            Console.WriteLine("6.");
+            Console.WriteLine("7.");
+            Console.WriteLine("8.");
+            Console.WriteLine("9.");
+            Console.WriteLine("10.");
+
+
+
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    temp1();
+                    break;
+
+                case 2:
+                    score();
+                    break;
+
+                case 3:
+                    prices();
+                    break;
+
+                case 4:
+                    FinishTimes();
+                    break;
+
+                case 5:
+                    grade();
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+                    break;
+
+
+            }
+        }
     }
 }
     
